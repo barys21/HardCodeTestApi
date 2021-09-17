1568655d74833e9e3bc9acb408b97a2673f9c8e6
Kenes Kuanyshov <kuanyshovk@gmail.com>
2021-09-17 11:26:07 +0600

    Added swagger

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,11 @@
 using HardCodeTestApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace HardCodeTestApi
 {
@@ -31,6 +25,9 @@ namespace HardCodeTestApi
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddSwaggerGen(options =>
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Conference Planner API", Version = "v1" }));
+
             services.AddControllers();
         }
 
@@ -41,6 +38,10 @@ namespace HardCodeTestApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Conference Planner API v1"));
 
             app.UseHttpsRedirection();
 
